1d8275eea6ccb93ff1b210bb8204cef49e0d0d00
arakul23 <babls2332@gmail.com>
2020-10-07 20:24:46 +0300

    Updated laravel version

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -1,4 +1,19 @@
 @include("layouts/head")
 @include("layouts/header")
-{{dd($students)}}
+<section id="next-section">
+    <div class="container-fluid">
+        <div class="row probootstrap-gutter0">
+            @foreach($students as $student)
+                <div class="col-md-4 col-sm-6">
+                    <a href="{{url('studentInfo', ['studentId' => $student->id])}}" class="probootstrap-hover-overlay">
+                        <img src="/public/img/noimg.jpg" alt="{{$student['name']." ".$student['surname']." ".$student['patronymic']}}" class="img-responsive">
+                        <div class="probootstrap-text-overlay">
+                            <h3>{{$student['name']." ".$student['surname']." ".$student['patronymic']}}</h3>
+                        </div>
+                    </a>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
 @include("layouts/footer")
